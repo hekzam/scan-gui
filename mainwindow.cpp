0db9d320f38fbc337cfc1846e54cb1e566d8fe27
Fabricot2406 <fabio.sj.yabar@gmail.com>
2024-04-07 19:12:10 +0200

    code: create layout foundations

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -2,68 +2,216 @@
 #include "mainwindow.h"
 static const QSize minPanelSize(250,500);
 
-MainWindow::MainWindow(QWidget *parent) : QMainWindow(parent)
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent)
 {
-  setMinimumSize(1280,720);
-  createMenuBar();
-  createSearchableColumns();
-  // createPreviewStack();
-  // createDockablePreview(); // floating preview
-  // createFixedPreview(); //docked preview
-  createPreviewPanel();
-  createWorkingView();
-  createMainStack();
+    setMinimumSize(1280,720);
+    createMenuBar();
+    createMainStack();
+    createWelcomeView();
+    createCreateProjectView();
+    createEvaluationView();
+}
+
+MainWindow::~MainWindow()
+{
 }
 
 void MainWindow::createMenuBar()
 {
-  // TODO : add QActions for each item of the menuBar
-  mainWindowMenuBar = menuBar(); // menuBar is not in any layout ?
-  currentViewTitle = new QLabel(tr("Name of the Window"),mainWindowMenuBar);
-  currentViewTitle->setIndent(10);
-  mainWindowMenuBar->setCornerWidget(currentViewTitle, Qt::TopLeftCorner);
-  fileMenu = new QMenu(tr("&Fichier"),mainWindowMenuBar);
-  editMenu = new QMenu(tr("&Édition"),mainWindowMenuBar);
-  viewMenu = new QMenu(tr("&Affichage"),mainWindowMenuBar);
-  helpMenu = new QMenu(tr("A&ide"),mainWindowMenuBar);
-  mainWindowMenuBar->addMenu(fileMenu);
-  mainWindowMenuBar->addMenu(editMenu);
-  mainWindowMenuBar->addMenu(viewMenu);
-  mainWindowMenuBar->addMenu(helpMenu);
-}
+    QMenuBar *menuBar = this -> menuBar();
 
-void MainWindow::createSearchableColumns()
-{
-  // placeholder QLabel
-  placeholderSearchableColumns = new QLabel("Not my problem, WIP",this);
-  // workingView->addWidget(placeholderSearchableColumns);
-  placeholderSearchableColumns->setMargin(50);
-  placeholderSearchableColumns->setMinimumSize(minPanelSize);
-}
+    // Menu Déroulant Fichier.
 
-void MainWindow::createPreviewPanel()
-{
-  previewPanelandButtons = new QGroupBox("the preview + the buttons here",this);
-  previewPanelandButtons->setMinimumSize(minPanelSize);
-}
+    fileMenu = menuBar -> addMenu(tr("&Fichier"));
 
-void MainWindow::createWorkingView()
-{
-  // TODO : add children to this box (preview, buttons and columns)
-  // TODO : check if the parent is correct
-  workingView = new QSplitter(this); //WIP
-  // workingView takes ownership with addWidget (see QSplitter docs)
-  workingView->addWidget(placeholderSearchableColumns);
-  workingView->addWidget(previewPanelandButtons);
-  workingView->setChildrenCollapsible(false);
+    QAction *newFileAction = new QAction(tr("&Nouveau"), this);
+    connect(newFileAction, &QAction::triggered, this, &MainWindow::openFileExplorer);
+    fileMenu -> addAction(newFileAction);
+
+    QAction *openFileAction = new QAction(tr("&Ouvrir"), this);
+    connect(openFileAction, &QAction::triggered, this, &MainWindow::openFileExplorer);
+    fileMenu -> addAction(openFileAction);
+
+    QAction *saveFileAction = new QAction(tr("&Enregistrer"), this);
+    connect(saveFileAction, &QAction::triggered, this, &MainWindow::openFileExplorer);
+    fileMenu -> addAction(saveFileAction);
+
+    // Menu Déroulant Edition.
+
+    editMenu = menuBar -> addMenu(tr("&Édition"));
+
+    QAction *undoAction = new QAction(tr("&Annuler"), this);
+    connect(undoAction, &QAction::triggered, this, &MainWindow::handleUndo);
+    editMenu -> addAction(undoAction);
+
+    QAction *redoAction = new QAction(tr("&Rétablir"), this);
+    connect(redoAction, &QAction::triggered, this, &MainWindow::handleRedo);
+    editMenu -> addAction(redoAction);
+
+    // Menu Déroulant Affichage.
+
+    viewMenu = menuBar -> addMenu(tr("&Affichage"));
+
+    QAction *darkModeAction = new QAction(tr("&Mode Sombre"), this);
+    connect(darkModeAction, &QAction::triggered, this, &MainWindow::toggleDarkMode);
+    viewMenu -> addAction(darkModeAction);
+
+    // Menu Déroulant Aide.
+
+    helpMenu = menuBar -> addMenu(tr("&Aide"));
+
+    QAction *userManualAction = new QAction(tr("&Manuel Utilisateur"), this);
+    connect(userManualAction, &QAction::triggered, this, &MainWindow::openUserManual);
+    helpMenu -> addAction(userManualAction);
 }
 
 void MainWindow::createMainStack()
 {
-  mainStack = new QStackedWidget(this);
-  mainStack->addWidget(workingView);
-  setCentralWidget(mainStack); // as it should be
+    mainStack = new QStackedWidget(this);
+    setCentralWidget(mainStack);
 }
 
-MainWindow::~MainWindow(){};
+void MainWindow::createWelcomeView()
+{
+    welcomeView = new QWidget(this);
 
+    QVBoxLayout *welcomeViewLayout = new QVBoxLayout(welcomeView);
+    welcomeViewLayout -> setAlignment(Qt::AlignCenter);
+
+    QLabel *titleLabel = new QLabel("<b>HEKZAM</b>", welcomeView);
+    titleLabel -> setAlignment(Qt::AlignCenter);
+    titleLabel -> setStyleSheet("font-size: 24px;");
+
+    QLabel *descriptionLabel = new QLabel("Logiciel d'évaluation d'examens.", welcomeView);
+    descriptionLabel -> setAlignment(Qt::AlignCenter);
+
+    QPushButton *createButton = new QPushButton("Créer un nouveau projet", welcomeView);
+    createButton -> setStyleSheet("font-size: 16px; background-color: green; color: white;");
+    createButton -> setFixedSize(250, 50);
+    connect(createButton, &QPushButton::clicked, this, &MainWindow::showCreateProjectView);
+
+    QPushButton *openButton = new QPushButton("Ouvrir un projet existant", welcomeView);
+    openButton -> setStyleSheet("font-size: 16px; background-color: blue; color: white;");
+    openButton -> setFixedSize(250, 50);
+    connect(openButton, &QPushButton::clicked, this, &MainWindow::openFileExplorer);
+
+    welcomeViewLayout -> addWidget(titleLabel);
+    welcomeViewLayout -> addWidget(descriptionLabel);
+    welcomeViewLayout -> addWidget(createButton);
+    welcomeViewLayout -> addWidget(openButton);
+
+    mainStack -> addWidget(welcomeView);
+}
+
+void MainWindow::createCreateProjectView()
+{
+    createProjectView = new QWidget(this);
+
+
+    QVBoxLayout *createProjectViewLayout = new QVBoxLayout(createProjectView);
+    createProjectViewLayout -> setAlignment(Qt::AlignCenter);
+
+    /**QGridLayout *fileGrid = new QGridLayout(createProjectView);
+    QHBoxLayout *repositoryLayout = new QHBoxLayout(createProjectView);
+    QHBoxLayout *examLayout = new QHBoxLayout(createProjectView);
+    QHBoxLayout *scanLayout = new QHBoxLayout(createProjectView);
+    QHBoxLayout *listLayout = new QHBoxLayout(createProjectView);
+
+    fileGrid -> addWidget(new QLabel("Pour continuer, veuillez renseigner les champs suivants.", createProjectView), 0, 0);
+    fileGrid -> addLayout(repositoryLayout, 1, 0);
+    fileGrid -> addLayout(examLayout, 2, 0);
+    fileGrid -> addLayout(scanLayout, 3, 0);
+    fileGrid -> addWidget(new QLabel("Si vous le souhaitez, vous pouvez également renseigner une liste d'émargement.", createProjectView), 0, 0);
+    fileGrid -> addLayout(listLayout, 5, 0);*/
+
+    QPushButton *openFileButton = new QPushButton("Ouvrir", createProjectView);
+    openFileButton -> setStyleSheet("font-size: 16px; background-color: orange; color: white;");
+    openFileButton -> setFixedSize(100, 50);
+    connect(openFileButton, &QPushButton::clicked, this, &MainWindow::showEvaluationView);
+
+    createProjectViewLayout -> addWidget(openFileButton);
+
+    mainStack -> addWidget(createProjectView);
+}
+
+void MainWindow::createEvaluationView()
+{
+    evaluationView = new QWidget(this);
+
+    // Premier split.
+
+    QHBoxLayout *evaluationViewLayout = new QHBoxLayout(evaluationView);
+    QSplitter *verticalSplitter = new QSplitter(evaluationView);
+    QSplitter *horizontalSplitter = new QSplitter(evaluationView);
+    QGroupBox *infoBox = new QGroupBox(evaluationView);
+
+    evaluationViewLayout -> addWidget(verticalSplitter);
+    verticalSplitter -> addWidget(horizontalSplitter);
+    verticalSplitter -> addWidget(infoBox);
+    verticalSplitter -> setOrientation(Qt::Vertical);
+    verticalSplitter -> setSizes(QList<int>() << 800 << 200);
+
+    // Deuxième split / partie haute.
+
+    QGroupBox *tableBox = new QGroupBox("Tableau de Tri", evaluationView);
+    QGroupBox *previewBox = new QGroupBox("Aperçu", evaluationView);
+
+    horizontalSplitter -> addWidget(tableBox);
+    horizontalSplitter -> addWidget(previewBox);
+
+    // Partie basse.
+
+    QHBoxLayout *infoLayout = new QHBoxLayout(infoBox);
+    QGroupBox *informationsBox = new QGroupBox("Informations Générales", evaluationView);
+    QVBoxLayout *informationsLayout = new QVBoxLayout(informationsBox);
+    QGroupBox *generalButtonsBox = new QGroupBox(evaluationView);
+    QHBoxLayout *generalButtonsLayout = new QHBoxLayout(generalButtonsBox);
+
+    infoLayout -> addWidget(informationsBox);
+    infoLayout -> addWidget(generalButtonsBox);
+    informationsLayout -> addWidget(new QLabel("WIP Informations",this));
+    generalButtonsLayout -> addWidget(new QLabel("WIP General Buttons",this));
+    generalButtonsBox -> setStyleSheet("QGroupBox { border: none; }");
+
+    mainStack -> addWidget(evaluationView);
+}
+
+void MainWindow::showWelcomeView()
+{
+    mainStack -> setCurrentWidget(welcomeView);
+}
+
+void MainWindow::showCreateProjectView()
+{
+    mainStack -> setCurrentWidget(createProjectView);
+}
+
+void MainWindow::showEvaluationView()
+{
+    mainStack -> setCurrentWidget(evaluationView);
+}
+
+void MainWindow::openFileExplorer()
+{
+    QString fileName = QFileDialog::getOpenFileName(this, tr("Open File"), QDir::homePath());
+    if (!fileName.isEmpty()) {
+        showEvaluationView();
+    }
+}
+
+void MainWindow::handleUndo()
+{
+}
+
+void MainWindow::handleRedo()
+{
+}
+
+void MainWindow::toggleDarkMode()
+{
+}
+
+void MainWindow::openUserManual()
+{
+}
--- mainwindow.h
+++ mainwindow.h
@@ -2,93 +2,49 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
-#include <QLabel>
-#include <QGroupBox>
-#include <QGridLayout>
-#include <QSplitter>
 #include <QStackedWidget>
-#include <QDebug>
+#include <QVBoxLayout>
+#include <QPushButton>
+#include <QLabel>
+#include <QFileDialog>
+#include <QMenuBar>
+#include <QMenu>
 
 class MainWindow : public QMainWindow
 {
-  Q_OBJECT
+    Q_OBJECT
 
 public:
-  MainWindow(QWidget *parent = nullptr);
-  ~MainWindow();
-  void deleteChildren(QObject *child);
-
-private:
-  void createMenuBar();
-  void createStatusBar(); // do we need one
-
-  void createWelcomeView(); // créer importer un projet
-  void createConfigView(); // configurer un nouveau projet (QDialog ??)
-  void createWorkingView(); // main working window
-  void createMainStack(); //holds the different views
-  // colonnes
-  void createSearchableColumns(); // not my problem XD
-  // preview
-  void createDockablePreview(); //holy sh*t do we really need this ??
-  void createFixedPreview(); // (help)
-  void createPreviewStack();
-  void createPreviewPanel();
-
-  // Menu Bar and children
-  QMenuBar *mainWindowMenuBar;
-  QLabel *currentViewTitle;
-  QMenu *fileMenu, *editMenu, *viewMenu, *helpMenu;
-
-  /**
-   * Ce QStackedWidget nous permettra d'afficher soit: (voir cahier des charges)
-   * - un écran nous permettant d'importer ou d'afficher un projet
-   * - un écran nous permettant de configurer le projet
-   * - un écran nous permettant de configurer
-   *    le programme (? ou une boite de dialogue)
-   * - la vue principale du programme
-   * - d'autres si besoin...
-   */
-  QStackedWidget *mainStack;
-
-  // Welcome Screen
-  QGroupBox *welcomeView; // ????
-
-  // Config Screen
-  QGroupBox *configView; // ??????? what to use here ?
-
-  // Main Window with columns and preview
-  QSplitter *workingView;
-
-  // part of the GUI dedicated to searching, filtering and displaying
-  // columns, placeholder for a real box in the future
-  QLabel *placeholderSearchableColumns;
-
-  // In this object we place the previews and the buttons
-  QGroupBox *previewPanelandButtons;
-
-  // we place the buttons here
-  QGridLayout *Buttons; //QButtonGroup ??
-
-  //Both previews have to be Stacked I guess ??? since we can show both
-  // the same time
-  QStackedWidget *previewStack;
-
-  // part of the GUI used to preview images, can be made to float
-  QDockWidget *floatablePreview;
-
-  /**
-   * part of the GUI that will show only when the floatablePreviewWidget
-   * is detached from the main window
-   */
-  QGroupBox *fixedPreview;
-
-
-
+    MainWindow(QWidget *parent = nullptr);
+    ~MainWindow();
 
 private slots:
-  // set the QLabel in the MenuBar to the correct title
-  // void setCurrentViewTitle();
-  // detach the preview
-  // void detachPreview();
+    void showWelcomeView();
+    void showCreateProjectView();
+    void showEvaluationView();
+    void openFileExplorer();
+    void handleUndo();
+    void handleRedo();
+    void toggleDarkMode();
+    void openUserManual();
+
+private:
+    void createMenuBar();
+    void createMainStack();
+    void createWelcomeView();
+    void createCreateProjectView();
+    void createEvaluationView();
+
+    QLabel *currentViewTitle;
+    QMenu *fileMenu;
+    QMenu *editMenu;
+    QMenu *viewMenu;
+    QMenu *helpMenu;
+    QStackedWidget *mainStack;
+
+    QWidget *welcomeView;
+    QWidget *createProjectView;
+    QWidget *evaluationView;
 };
+
 #endif // MAINWINDOW_H

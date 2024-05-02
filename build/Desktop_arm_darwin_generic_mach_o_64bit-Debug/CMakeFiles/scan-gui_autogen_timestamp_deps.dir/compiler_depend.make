9a95fd28bbe8a66bfcc5a31e5d89673b28f6c439
Marco <marcomartins@MacBook-Pro-de-Marco.local>
2024-05-03 01:35:58 +0200

    code : added linkage between images and corresponding json file

--- build/Desktop_arm_darwin_generic_mach_o_64bit-Debug/CMakeFiles/scan-gui_autogen_timestamp_deps.dir/compiler_depend.make
+++
@@ -1,2 +0,0 @@
-# Empty custom commands generated dependencies file for scan-gui_autogen_timestamp_deps.
-# This may be replaced when dependencies are built.
--- build/Qt_6_7_0_for_macOS-Debug/.qt/QtDeployTargets.cmake
+++
@@ -1,1 +0,0 @@
-set(__QT_DEPLOY_TARGET_scan-gui_FILE /Users/marcomartins/Hekzam2/scan-gui/build/Qt_6_7_0_for_macOS-Debug/scan-gui.app/Contents/MacOS/scan-gui)
